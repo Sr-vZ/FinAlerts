2ec5b1ef9fbbaef2e5641cfc07153d512fcb0188
srvz <srvz39@gmail.com>
2024-11-24 20:20:29 +0000

    feat: ui pages routes added

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,8 +11,12 @@
 
 # main.py
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, RedirectResponse, Response
+from fastapi.templating import Jinja2Templates
+import os
 
 from .routers import auth
 from .routers import user
@@ -31,6 +35,22 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# load templates
+# env = jinja2.Environment(loader=PackageLoader("./../../ui"))
+app_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+templates = Jinja2Templates(directory=f"{app_root}/ui")
+# templates = env.get_template()
+# templates = Jinja2Templates(directory=Path(__file__).parent.parent+ "ui/")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    print(app_root)
+    return templates.TemplateResponse("index.html",
+     {"request": request})
 
 app.include_router(user.router)
 app.include_router(auth.router, prefix="/auth")
