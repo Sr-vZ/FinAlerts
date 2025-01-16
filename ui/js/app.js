01912cecbacf40d2cc8985b26f2f2f13eb81c733
srvz <srvz39@gmail.com>
2025-01-16 20:39:03 +0000

    fix: get ohlc_data

--- ui/js/app.js
+++ ui/js/app.js
@@ -29,11 +29,33 @@ function periodic_updates() {
 
 async function index_ticker(symbol) {
     // document.getElementById("index-ticker")
-    res = await fetch("nse_indices?symbol=" + symbol)
+    res = await fetch("/nse_indices?symbol=" + symbol)
     data = await res.json()
     console.log(data)
     document.getElementById("index-ticker").querySelector('.stat-value').innerHTML = data['LTP']
     document.getElementById("index-ticker").querySelector('.stat-title').innerHTML = data['Name']
+}
+
+async function ohlc_data(symbol) {
+    date = new Date()
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    let mm = today.getMonth() + 1; // Months start at 0!
+    let dd = today.getDate();
+
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+
+    const end_date = dd + '-' + mm + '-' + yyyy;
+    dd = dd - 7
+    if (dd < 10) dd = '0' + dd
+    const st_date = dd + '-' + mm + '-' + yyyy;
+    // end_date = `${date.getDay()}-${date.getMonth() + 1}-${date.getFullYear()}`
+    // st_date = `${parseInt(date.getDay()) - 7}-${date.getMonth() + 1}-${date.getFullYear()}`
+    res = await fetch(`/indices/?symbol=NIFTY%2050&startdate=${st_date}&enddate=${end_date}&realtime=false`)
+    data = await res.json()
+    console.log(data)
+
 }
 
 periodic_updates()
